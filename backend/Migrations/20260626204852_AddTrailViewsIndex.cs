5a148a5424e1f30d77b4e72a1706c8bd6bb9cb05
svanur <svanur.karlsson@gmail.com>
2026-06-26 20:53:39 +0000

    Add `ElevationProfile` to `GetTrailBySlug` response and create migration for trail views index

---
+++ backend/Migrations/20260626204852_AddTrailViewsIndex.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Utanvega.Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTrailViewsIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_TrailViews_TrailId",
+                table: "TrailViews",
+                column: "TrailId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_TrailViews_TrailId",
+                table: "TrailViews");
+        }
+    }
+}
